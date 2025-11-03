6b0e3b3226db8e07600e2491ea0acccb8b3a36e0
riflowth <riflowth@gmail.com>
2025-11-03 19:52:50 +0700

    fix: wrong network interface

--- provision/user_data/setup-k3s-master-init.sh
+++ provision/user_data/setup-k3s-master-init.sh
@@ -2,3 +2,4 @@
 
 curl -sfL https://get.k3s.io | K3S_TOKEN=${k3s_token} sh -s - \
   --disable traefik \
+  --flannel-iface eth1 \
--- provision/user_data/setup-k3s-worker.sh
+++ provision/user_data/setup-k3s-worker.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-curl -sfL https://get.k3s.io | K3S_TOKEN=${k3s_token} K3S_URL=https://${k3s_master_ip}:6443 sh -s -
+curl -sfL https://get.k3s.io | K3S_TOKEN=${k3s_token} K3S_URL=https://${k3s_master_ip}:6443 sh -s - \
+  --flannel-iface eth1 \
